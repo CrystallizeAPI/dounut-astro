cb9d3f564543e2e94f2b2905d60e792768957ad7
dhairyadwivedi <dhairiya96@gmail.com>
2024-02-23 15:02:29 +0100

    update footer and logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,11 @@
 import { BasketButton } from "./basket-button";
-import { Logo } from "./logo";
+import AstroLogo from "../assets/astro-logo.svg";
 
 export const Header = () => {
     return (
         <header className="container flex justify-between mx-auto py-10 w-full">
             <a href="/" title="AstroJS">
-                <Logo />
+                <img src={AstroLogo.src} alt="Dount and Astro logo" className="w-24" />
             </a>
             <a href="/cart" title="Your cart">
                 <BasketButton />
